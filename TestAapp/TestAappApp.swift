d267466a931bc49f99f0619de0e53e91021c75bc
Zai Fuchigami <zai@siliconrecord.com>
2023-12-15 18:53:17 +0900

    Initial Commit

---
+++ TestAapp/TestAappApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAappApp.swift
+//  TestAapp
+//
+//  Created by Zai Fuchigami on 2023/12/15.
+//
+
+import SwiftUI
+
+@main
+struct TestAappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
